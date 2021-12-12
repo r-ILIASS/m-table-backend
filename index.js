2b08d1c73d283f66073e5c5feffed897f77202c9
r-ILIASS <iliass.root@gmail.com>
2021-12-12 20:00:01 +0100

    feat: add authentication and authorization endpoints/middleware

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const movies = require("./routes/movies");
 const express = require("express");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey isn ot defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/movies")
@@ -18,6 +26,8 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
