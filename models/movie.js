ca09df5c894be068811801ab14f85201c947a870
r-ILIASS <iliass.root@gmail.com>
2021-12-16 19:43:58 +0100

    fix[movie model]: id validation error

--- models/movie.js
+++ models/movie.js
@@ -34,7 +34,7 @@ const Movie = mongoose.model(
 function validateMovie(movie) {
   const schema = Joi.object({
     title: Joi.string().min(5).max(50).required(),
-    genreId: Joi.objectId().required(),
+    genreId: Joi.string().required(),
     numberInStock: Joi.number().min(0).required(),
     dailyRentalRate: Joi.number().min(0).required(),
   });
